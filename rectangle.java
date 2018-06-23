2c1279e3947c67e72ceb10b2e7223462a9446b58
AishwaryaChaudharyQAIT <40285334+AishwaryaChaudharyQAIT@users.noreply.github.com>
2018-06-23 20:20:03 +0530

    Update rectangle.java

--- rectangle.java
+++ rectangle.java
@@ -1,41 +1,40 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NameSplit {
+import org.omg.CORBA.TRANSACTION_UNAVAILABLE;
 
-public static void main( String[] args )
-{
-
-
-@SuppressWarnings("resource")
-Scanner sc=new Scanner(System.in); 
-System.out.println("enter Fisrt Name");
-String firstName= sc.nextLine();
-System.out.println("enter last name");
-String secondName= sc.nextLine();
-char[] fname = firstName.toCharArray(); 
-char[] sname = secondName.toCharArray();
-
-
-char[] result = new char[fname.length + sname.length];
-int j=0,k=0;
-for(int i= 0;i<result.length;)  {
-if(j<fname.length){
-result[i++] =fname[j++];
-
-}
-if(k<sname.length){
-result[i++] =sname[k++];
-}
-}
-for(int i= 0;i<result.length ;i=i+2){
-
-System.out.print(result[i]);
-System.out.print(result[i+1] + " ");
-
-
+public class GivenSidesRectangleOrNot {
+	public static void main(String[]agrs)
+	{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter value of a,b,c,d");
+int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt(),d=sc.nextInt();
+if(isRectangle(a,b,c,d))
+{System.out.println("Yes its a Rectangle");
+	}
+else
+{System.out.println("Not a rectangle");
 }
 
+}
+	static boolean isRectangle(int a,int b ,int c,int d)
+	{
+		if(a==b && b==c && c==d && d==a)
+		{return true;
+	}
+		else if(a==b && c==d)
+		{return true;}
+		
+	else if(a==c && b==d)
+	{
+		return true;
+	}
+	else if(a==d && b==c)
+	{
+	return true;	
+	}
+	else
+		return false;
 
 }
 }
+
